06220916a152d0a8678dc7e5791385e72c074f7f
Kim Björnsen Åklint <89244587+Bjornsen016@users.noreply.github.com>
2022-08-31 15:26:49 +0200

    First lesson

--- 1-lektion/zassignment.js
+++ 1-lektion/zassignment.js
@@ -1,3 +1,5 @@
+const data = require("./data.json");
+
 /**
  1. create an object of entries about personal information
  * it should necessarily contain the following entries along with the others:
@@ -10,10 +12,16 @@
 const info = {
 	// your entries here
 	name: "Erik Andersson",
-}
+	birthDay: new Date(`2000-11-11`),
+	toys: ["car", "bike", "boat"],
+	partner: { name: "Erika Andersson" },
+	age: function age() {
+		Math.round((new Date() - this.birthDay) / 1000 / 60 / 60 / 24 / 365);
+	}S,
+};
 
-// console.log(info.name)
-
+console.log(info);
+console.log(info.age());
 
 /**
  * 2. from the array inside the object "info", add it to a new array using destructuring ellipses.
@@ -21,45 +29,39 @@ const info = {
  * ["element-one", rest of the contents of the old array, "element-two"]
  */
 
-const newArray = []
+const newArray = ["element-one", ...info.toys, "elemtent-two"];
 
-// console.log(newArray)
-
+console.log(newArray);
 
 /**
  * 3. create a json file containing an object with at least one entry.
  * Add the object from the file into a new field called "new" inside the object "info"
  */
 
-info.new = {}
+info.new = { data };
 
-// console.log(info)
-
+console.log(info);
 
 /**
  * 4. create an arrow function which creates and returns the list of keys from an input object
  */
 
-const getKeys = () => {}
-
-// console.log(getKeys(info))
-
+const getKeys = (obj) => Object.keys(obj);
+console.log(getKeys(info));
 
 /**
  * 5. convert the date entry from the object "info" into an ISO string and parse it
  */
-const myDateString = ""
-
-// console.log(myDateString)
-
+const myDateString = Date.parse(info.birthDay.toISOString());
+console.log(myDateString);
 
 /**
- * 6. take the array from the object "info" and use the "map" function to return a 
+ * 6. take the array from the object "info" and use the "map" function to return a
  * new array with each item being converted into a string
  */
-const myNewArray = []
+const myNewArray = info.toys.map((toy) => toy.toString());
 
-// console.log(myNewArray)
+console.log(myNewArray);
 
 /**
  * 7. try to understand and experiement with the following functions (can ask me for any help always):
@@ -67,3 +69,11 @@ const myNewArray = []
  * - reduce function (array)
  * - filter function (array)
  */
+
+const arrRed = info.toys.reduce((theToys, toy) => (theToys += `, ${toy}`));
+
+console.log(arrRed);
+
+const arrFilter = info.toys.filter((toy) => toy === "car");
+
+console.log(arrFilter);
